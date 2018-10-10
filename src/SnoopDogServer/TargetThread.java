4f91a24138f9998b274d563126b56873742d7be3
Liore Finkelstein <liorefinkel@gmail.com>
2018-10-10 13:40:38 +0300

    Add Target thread for recording

---
+++ src/SnoopDogServer/TargetThread.java
@@ -0,0 +1,49 @@
+package SnoopDogServer;
+
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+import java.io.ByteArrayOutputStream;
+
+public class TargetThread extends Thread {
+    private TargetDataLine targetLine;
+    private ByteArrayOutputStream out;
+    private volatile boolean shouldRun = true;
+
+    public TargetThread(TargetDataLine targetLine, ByteArrayOutputStream out) {
+        super("TargetThread");
+        this.targetLine = targetLine;
+        this.out = out;
+    }
+
+    private void resumeListening() {
+        targetLine.flush();
+        targetLine.start();
+    }
+
+    public void startRecording() {
+        System.out.println("Start recording...");
+        targetLine.start();
+        byte[] data = new byte[targetLine.getBufferSize() / 5];
+        int readBytes;
+        while (shouldRun) {
+            readBytes = targetLine.read(data, 0, data.length);
+            out.write(data, 0, readBytes);
+        }
+
+    }
+
+    public void stopRecording() {
+        System.out.println("Stop recording...");
+        targetLine.stop();
+    }
+
+    @Override
+    public void run() {
+        startRecording();
+    }
+
+    public void kill() {
+        this.shouldRun = false;
+    }
+}
